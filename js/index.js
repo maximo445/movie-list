d39ed6d67b9acfd7634d2b2d3fee923505dc5241
Maximo Brito <britofilmmaker@gmail.com>
2023-11-19 18:08:40 -0500

    working on adding to list ui functionallity

--- js/index.js
+++ js/index.js
@@ -1,44 +1,175 @@
 class App {
+
     constructor(){
         this.ui = new UI();
+        
+        // check for load and page changes
         window.addEventListener('hashchange', this._route.bind(this));
-        window.addEventListener('load', this._route.bind(this))
-    }
+        window.addEventListener('load', this._route.bind(this));
 
+        //display add form
+    }
+    
     _route() {
+        
         const pathName = window.location.pathname;
-        console.log(pathName);
+        
         switch (pathName) {
             case '/index.html':
-                console.log(this);
+                this.ui.displayPopularMovies();          
                 break;
         
             default:
                 break;
         }
+
     }
 }
 
 class UI {
-    displayPopularMovies() {
+
+    constructor() {
+        this._fetcher = new Fetcher();
+        this._filmTracker = new FilmsTracker();
+    }
+
+    _addFilm(e) {
+
+        e.preventDefault();
+
+        let name = '';
+        let id ='';
+
+        if (e.target.classList.contains('fas')) {
+            name = e.target.parentElement.getAttribute('data-name');
+            id = e.target.parentElement.getAttribute('data-id');            
+        } else {
+            name = e.target.getAttribute('data-name');
+            id = e.target.getAttribute('data-id');
+        }
+
+        console.log('from form: ', {name, id});
 
     }
+
+    _closePopUp() {
+        document.querySelector('#add-to-list-popup').style.display = 'none';
+    }
+
+    _showCreateList() {
+        
+        document.querySelector('#create-list').style.display = 'block';
+    }
+
+    _displayAddListPopUp(e) {
+        
+        let name = 'none';
+        let id ='123456';
+
+        if (e.target.classList.contains('fas')) {
+            name = e.target.parentElement.getAttribute('data-name');
+            id = e.target.parentElement.getAttribute('data-id');            
+        } else {
+            name = e.target.getAttribute('data-name');
+            id = e.target.getAttribute('data-id');
+        }
+
+        // dinamically create popUp element
+
+        const popUp  = document.querySelector('#add-to-list-popup');
+
+        popUp.innerHTML = ``;
+         
+        const div = document.createElement('div');
+        div.classList.add('container');
+
+        div.innerHTML = `
+            <div class="close-container">
+                <button class="close-popup">X</button>
+            </div>
+            <form id="add-to">
+                <input type="submit" value="Add">
+                <br>
+                ${this._filmTracker.lists.map(list => {
+                    return `
+                        <label>
+                            <input type="checkbox" data-it="${list.id}" name="${list.name}" value="${list.name}"> Option 1
+                        </label>                    
+                    `;
+                }).join('<br>')}
+            </form>
+            <button id="display-create-list"> + Create List</button>
+            <form id="create-list">
+                <label for="field1">Name</label>
+                <br>
+                <input type="text" id="field1" name="field1" placeholder="Enter playlist name...">
+                <br>
+                <input type="submit" value="Create">
+            </form>                                              
+        `;
+
+        
+        popUp.appendChild(div);
+        popUp.style.display = 'flex';
+
+        const addToForm = document.querySelector('#add-to');
+
+        addToForm.setAttribute('data-name', name);
+        addToForm.setAttribute('data-id', id);
+
+        document.querySelector('.close-popup').addEventListener('click', this._closePopUp.bind(this));
+        document.querySelector('#display-create-list').addEventListener('click', this._showCreateList.bind(this));
+        document.querySelector('#add-to').addEventListener('submit', this._addFilm.bind(this));
+    }
+
+    async displayPopularMovies() {
+
+        const {results} = await this._fetcher.getPopularMovies();
+
+        console.log(results);
+
+        results.forEach(movie => {
+            const div = document.createElement('div');
+            div.classList.add('card-item');
+            div.innerHTML = `
+                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="film-poster">
+                <h2 class="title">${movie.title}</h2>
+                <div class="bottom">
+                    <p class="rating">rating: ${movie.vote_average}</p>
+                    <button data-name="${movie.title}" data-id="${movie.id}" class="display-list-form"><i class="fas fa-plus"></i></button>
+                </div>
+            `;
+            document.querySelector('#popular-movies .film-container').appendChild(div);
+        });
+
+        const popupButtons =document.querySelectorAll('.display-list-form');
+
+        popupButtons.forEach(button => {
+            button.addEventListener('click', this._displayAddListPopUp.bind(this));
+        })
+
+    }
+
 }
 
-class Delagator {
-    // manages adding to list
+// film class
+class Film {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+    }
 }
 
 // list crwd
 class List {
-    constructor() {
+    constructor(name) {
         this.id = 'produce new id';
-        this.name = '';
+        this.name = name;
         this.films = [];
     }
 
     addFilm(film) {
-
+        this.films.append(film);
     }
 
     removeFilm(film) {
@@ -57,7 +188,8 @@ class FilmsTracker {
     }
 
     createList (name) {
-
+        const newList = new List(name);
+        this.lists.append(newList);
     }
 
     modifyList (action, film) {
